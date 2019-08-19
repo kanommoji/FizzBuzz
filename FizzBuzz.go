5ada3b4dad9f41efa223d11c72926e520ced4c52
sck shuhari <sckshuhari@scks-MacBook-Pro.local>
2019-08-19 10:42:38 +0700

    [Added]: Unit test กรณี Input 6,15 ใน FizzBuzz_test.go และ refactor code ใน FizzBuzz.go

--- FizzBuzz.go
+++ FizzBuzz.go
@@ -1,10 +1,19 @@
 package fizzbuzz
 
+import "strconv"
+
 func ConvertNumberToFizzBuzz(number int) string {
 	var stringNumber string
-	stringNumber = "1"
+	stringNumber = strconv.Itoa(number)
 	if number%3 == 0 {
 		stringNumber = "Fizz"
 	}
+	if number%5 == 0 {
+		stringNumber = "Buzz"
+	}
+	if number%15 == 0 {
+		stringNumber = "FizzBuzz"
+	}
 	return stringNumber
+
 }
--- FizzBuzz_test.go
+++ FizzBuzz_test.go
@@ -23,3 +23,36 @@ func Test_ConvertNumberToFizzBuzz_Input_3_Should_Be_Fizz(t *testing.T) {
 		t.Errorf("Expect %v but got %v", expected, actual)
 	}
 }
+
+func Test_ConvertNumberToFizzBuzz_Input_5_Should_Be_Buzz(t *testing.T) {
+	expected := "Buzz"
+	number := 5
+
+	actual := ConvertNumberToFizzBuzz(number)
+
+	if expected != actual {
+		t.Errorf("Expect %v but got %v", expected, actual)
+	}
+}
+
+func Test_ConvertNumberToFizzBuzz_Input_6_Should_Be_Fizz(t *testing.T) {
+	expected := "Fizz"
+	number := 6
+
+	actual := ConvertNumberToFizzBuzz(number)
+
+	if expected != actual {
+		t.Errorf("Expect %v but got %v", expected, actual)
+	}
+}
+
+func Test_ConvertNumberToFizzBuzz_Input_15_Should_Be_FizzBuzz(t *testing.T) {
+	expected := "FizzBuzz"
+	number := 15
+
+	actual := ConvertNumberToFizzBuzz(number)
+
+	if expected != actual {
+		t.Errorf("Expect %v but got %v", expected, actual)
+	}
+}
